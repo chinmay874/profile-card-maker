278815341004531245f0e9e416f9c95b553aa626
chinmay jain <chinmay874@gmail.com>
2023-04-27 17:46:24 +0530

    List and key problem

--- src/App.js
+++ src/App.js
@@ -1,17 +1,35 @@
 //import logo from './logo.svg';
 import './App.css';
 import {Card} from './component/card.js';
-import logo1 from './boy.png';
-import logo2 from './man.png';
-import logo3 from './ganja.png'
 
 
+const perosnInfo=[
+{
+img:"boy.png",
+name:"Chinmay jain", 
+designation:"FrontEnd developer",
+description:"As a frontend developer, I specialize in crafting user-friendly interfaces for websites and web applications. Using my expertise in HTML, CSS, JavaScript, and other frontend technologies."
+},
+{
+img:"ganja.png",
+name:"Rahul",
+designation:"BackEnd developer",
+description:"As a backend developer, I specialize in building the server-side logic and infrastructure that powers websites and web applications. Using my expertise in programming languages such as Java, Python, or Ruby"
+},
+{
+img:"man.png",
+name:"Sandeep",
+designation:"FullStack developer",
+description:"As a fullstack developer, I am proficient in both frontend and backend technologies, allowing me to build end-to-end web applications. "
+},
+]
 function App() {
   return (
     <div className="App">
-      <Card img={logo1} name="Chinmay jain" designation="FrontEnd developer" description="As a frontend developer, I specialize in crafting user-friendly interfaces for websites and web applications. Using my expertise in HTML, CSS, JavaScript, and other frontend technologies."/>
-      <Card img={logo2} name="Rahul" designation="BackEnd developer" description="As a backend developer, I specialize in building the server-side logic and infrastructure that powers websites and web applications. Using my expertise in programming languages such as Java, Python, or Ruby"/>
-      <Card img={logo3} name="Sandeep" designation="FullStack developer" description="As a fullstack developer, I am proficient in both frontend and backend technologies, allowing me to build end-to-end web applications. "/>
+      {/* <Card  person={perosnInfo[0]}/> */}
+      {/* <Card  person={perosnInfo[1]}/>
+      <Card  person={perosnInfo[2]}/> */}
+      {[perosnInfo[0],perosnInfo[1],perosnInfo[2]].map((obj,i) => <Card key={i*10} person={obj}/>)}
     </div>
   );
 }
--- src/component/card.js
+++ src/component/card.js
@@ -2,15 +2,15 @@ import { Fragment } from 'react'
 import './card.css'
 export function Card(props){
     function btn(){
-        alert(props.name)
+        alert(props.person.img)
     }
     return(
         <Fragment>
             <div id="card1">
-                <img src={props.img} style={{border:'3px solid #ffffff', borderRadius:'50%'}}/>
-                <p style={{color:'#ffffff', fontSize:'15px',fontFamily:'sans-serif'}}>{props.designation}</p>
-                <p style={{color:'#ffffff',fontSize:"20px", fontWeight:'bold'}}>{props.name}</p>
-                <p style={{color:'#ffffff',fontSize:'13px'}}>{props.description}</p>
+                <img id="img1" src={props.person.img} alt='' style={{border:'3px solid #ffffff', borderRadius:'50%'}}/>
+                <p style={{color:'#ffffff', fontSize:'15px',fontFamily:'sans-serif'}}>{props.person.designation}</p>
+                <p style={{color:'#ffffff',fontSize:"20px", fontWeight:'bold'}}>{props.person.name}</p>
+                <p style={{color:'#ffffff',fontSize:'13px'}}>{props.person.description}</p>
                 <button onClick={btn} style={{height:'30px',width:'100px',borderRadius:'15px'}}>See More</button>
 
             </div>
